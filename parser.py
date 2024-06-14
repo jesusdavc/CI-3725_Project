63d978f85c500c03989cf7efdb45f3ff0aa9c591
dev-jlpt18 <jlpt.1910@gmail.com>
2024-06-14 17:18:31 -0400

    Integracion del reconocedor para array

--- parser.py
+++ parser.py
@@ -28,16 +28,18 @@ def p_expresion_declare(p):
     '''declare : TkDeclare expresion'''
     
     p[0] = Declare("declare", p[2])
-    print("declare: "+ p[1] +", "+ p[2].type )
+    print("declare: " + p[2].type )
 
 # Produccion para detectar la expresion las secuencias del programa
 def p_expresion_semicolon(p):
     '''expresion : expresion TkSemicolon expresion
                  | expresion TkAsig expresion
                  | expresion TkAsig number
+                 | soForth
                  | twoPoints 
                  | space
                  | Word
+                 | number
                  | Empty '''
     
     if len(p) > 2:
@@ -49,7 +51,7 @@ def p_expresion_semicolon(p):
             p[0] = Secuencia("Secuencia", p[1],p[3])
             print("semicolon nivel: "+p[1].type + ";" + p[3].type)
     else:
-        p[0] = toAtom("Rumbo Atomico: ",p[1])
+        p[0] = toAtom("RA",p[1])
 
 
 # Produccion para detectar la expresion no terminal TwoPoints
@@ -61,28 +63,47 @@ def p_expresion_two_point(p):
     p[0] = TwoPoints("TwoPoints: ", p[1], p[3])
     print("dos puntos nivel: "+p[1].type +","+ p[3].type) 
 
+# Produccion para detectar la expresion no terminal SoForth
+def p_expresion_so_forth(p):
+    '''soForth : number TkSoForth number'''
+
+    p[0] = TwoSoFort("TkSoForth: ", p[1], p[3])
+    print(".. nivel: "+p[1].type +","+ p[3].type) 
+
 # Produccion para detectar el especio vacio en la declaracion
 def p_expresion_space_empty(p):
     '''space : twoPoints expresion'''
     p[0] = Space_Declare("Space: ", p[1], p[2])
-    print("empacio vacio: "+p[1].type +","+ p[2].type) 
+    print("space: "+p[1].type +","+ p[2].type) 
 
 
 # Produccion para detectar las palabras reservadas
 def p_reserved(p):
     '''reserved : TkInt
-                | TkBool'''
+                | TkBool
+                | TkArray readArray'''
     
     if (len(p) > 2):
-        p[0] = Array("Array: ", p[3], p[5])
-        print("Array: "+ p[1] +","+ p[2].type)
+        p[0] = Array("Array", p[1], p[2])
+        print("Array: "+ p[1])
     elif (p[1] == 'int'):
-        p[0] = Reserved("TkInt: ", p[1])
-        print("Reservado: "+ p[1])
+        p[0] = Reserved("TkInt: ", value = p[1])
+        print("Reservado TkInt: "+ p[1])
     else:
-        p[0] = Reserved("TkBool: ", p[1])
-        print("Reservado: "+ p[1])
+        p[0] = Reserved("TkBool: ", value = p[1])
+        print("Reservado bool: "+ p[1])
 
+# Produccion para leer un array
+def p_read_array(p):
+    '''readArray : expresion TkOBracket expresion TkCBracket
+                 | TkOBracket expresion TkCBracket'''
+    
+    if (len(p) > 4):
+        p[0] = ReadArray("ReadArray", p[1], p[3])
+        print("ReadArray x[]: "+ str(p[1].type) +".."+ str(p[3].type))
+    else:
+        p[0] = ReadArray("ReadArray", p[2])
+        print("ReadArray []: "+ str(p[2].type) )
 # Produccion para detectar la expresion terminal de un numero
 def p_number(p):
     '''number : TkNum'''
@@ -98,7 +119,7 @@ def p_expresion_id(p):
 # Produccion para detectar la expresion terminal vacia o letra
 def p_expresion_empty(p):
     '''Empty : '''
-    p[0] = Atom("Vacio:" )
+    p[0] = Atom("Empty" )
     print("Vacio: "+ p[0].type) 
 
 # Produccion para detectar la expresion comentario
@@ -114,25 +135,36 @@ def p_error(p):
 #Inicializador del parser
 parser = yacc.yacc()
 
-
 #Clase para la creacion de nodos, con el fin de generar el arbol AST
 class Atom:
 
     def __init__(self, type, value=None):
         self.type = type
         self.value = str(value)
+
     def print_AST(self, level=0):
-        if(self.value == None):
+        if (self.type == "Empty"):
+            AST= ""
+        elif(self.value == None):
             AST = "-"*level + self.type
         else:
             AST = "-"*level + self.type +self.value
         print(AST)
+
+    def print_AST_DQ(self):
+        #res = "Estoy en ATOM DQ, valor:"+ str(self.value) 
+        #print(res)
+        pila = deque()
+        pila.append(self.value)
+        return pila
 
 class Reserved:
 
-    def __init__(self, type, value=None):
+    def __init__(self, type, left=None, right=None, value=None):
         self.type = type
         self.value = value
+        self.left = left
+        self.right = right
 
     def print_AST(self, level=0):
         if(self.value == None):
@@ -140,6 +172,19 @@ class Reserved:
         else:
             AST = "-"*level + self.type +self.value
         print(AST)
+    
+    def print_AST_DQ(self):
+        #res = "Estoy en ATOM DQ, valor:"+ str(self.value) 
+        #print(res)
+        '''if (self.type == "Read"):
+            pila = deque()
+            pila.append('array')
+            pila += self.rigth.print_AST_DQ()
+            return pila
+        else:'''
+        pila = deque()
+        pila.append(self.value)
+        return pila
 
 class Secuencia:
 
@@ -199,10 +244,26 @@ class TwoPoints:
         self.right = right
 
     def print_AST(self, level=0):
-        AST = "-"*level + self.type
-        print(AST)
-        self.left.print_AST(level+1)
-        self.right.print_AST(level+1)
+        #res = "Estoy en AST Twopoint, el hijo izquierdo es: "+ str(self.left.value)
+        #print(res) 
+        #res = "Estoy en AST Twopoint, el hijo derecho es: "+ self.right.type
+        #print(res)
+        if (self.right.value == "int" or self.right.value == "bool" 
+            or self.right.type == "Array"): 
+            #print("Estoy en AST TwoPoint QD")
+            pila = deque()
+            AST = "-"*level
+            pila += self.left.print_AST_DQ()
+            pila.append(":")
+            pila += self.right.print_AST_DQ()
+            AST += " ".join(pila)
+            print(AST)
+        else:
+            AST = "-"*level + self.type
+            print(AST)
+            self.left.print_AST(level+1)
+            self.right.print_AST(level+1)
+
 
 class Asignation:
 
@@ -231,17 +292,62 @@ class Space_Declare:
 
 class Array:
 
+    def __init__(self, type, value=0, right=None):
+        self.type = type
+        self.right = right
+        self.value = value
+
+    def print_AST_DQ(self, level=0):
+        #print("Estoy en AST ARRAY DQ")
+        #print(self.value)
+        pila = deque()
+        pila.append(self.value)
+        pila += self.right.print_AST_DQ()
+        return pila
+
+    
+class ReadArray:
+
     def __init__(self, type, left=None, right=None):
         self.type = type
         self.left = left
         self.right = right
 
     def print_AST(self, level=0):
-        AST = "-"*level + self.type
-        print(AST)
+        #print("Estoy en AST READ")
+        ret = "-"*level + self.type+":"
+        print(ret)
         self.left.print_AST(level+1)
         self.right.print_AST(level+1)
-    
+
+    def print_AST_DQ(self):
+        #print("Estoy en AST DQ READ")
+        pila = deque()
+        pila.append("[")
+        pila += self.left.print_AST_DQ()
+        pila.append("]")
+        return pila
+
+  
+class TwoSoFort:
+
+    def __init__(self, type, left=None, right=None):
+        self.type = type
+        self.left = left
+        self.right = right
+
+    def print_AST(self, level=0):
+        ret = "-"*level + self.type+":"
+        print(ret)
+        self.left.print_AST(level+1)
+        self.right.print_AST(level+1)
+
+    def print_AST_DQ(self):
+        pila = deque()
+        pila += self.left.print_AST_DQ()
+        pila.append("..")
+        pila += self.right.print_AST_DQ()
+        return pila
 
 class Declare:
 
@@ -274,8 +380,12 @@ class toAtom:
         self.level = level
         self.children = children
         self.value = value
+
     def print_AST(self, level=0):
-        self.children.print_AST(level+1)
+        self.children.print_AST(level)
+
+    def print_AST_DQ(self):
+        return self.children.print_AST_DQ()
     
 while True:
     f = open(sys.argv[1], "r")   
