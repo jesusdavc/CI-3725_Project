cf0b741c812ac6cd3d9accdd2920bc4ed29f3835
dev-jlpt18 <jlpt.1910@gmail.com>
2024-06-15 02:30:28 -0400

    Cambios en la precedencia y reconoce if

--- parser.py
+++ parser.py
@@ -12,12 +12,16 @@ if ".." not in sys.path: sys.path.insert(0,"..") # type: ignore
 
 # Precedencia de algunas expresiones de GCL
 precedence = (
-    ('left', 'TkTo'),
-    ('right', 'TkIn'),
-    ('left', 'TkComma', 'TkConcat'),
-    ('left', 'TkTwoPoints', 'TkAsig'),
-    ('left', 'TkOpenPar', 'TkClosePar'),
-    ('right', 'TkSemicolon'),
+    ('right', 'TkSemicolon', 'TkGuard'),
+    ('left', 'TkTwoPoints'),
+    ('left', 'TkAnd'),
+    ('left', 'TkOr'),
+    ('left', 'TkLess', 'TkLeq', 'TkGeq', 'TkGreater', 'TkEqual', 'TkNEqual'),
+    ('left', 'TkConcat'),
+    ('left', 'TkIn'),
+    ('right', 'TkTo'),
+    ('right', 'TkAsig'),
+    ('left', 'TkComma'),
 )
 
 # Produccion para detectar un programa en GCL
@@ -36,15 +40,18 @@ def p_expresion_declare(p):
 # Produccion para detectar la expresion las secuencias del programa
 def p_expresion_semicolon(p):
     '''expresion : expresion TkSemicolon expresion
-                 | expresion TkAsig expresion
-                 | expresion TkAsig number
                  | print
                  | soForth
                  | concat
                  | writeArray
+                 | readArray
                  | twoPoints
                  | bucle
-                 | condicion
+                 | cota
+                 | condition
+                 | booleans
+                 | bools
+                 | asig
                  | comma 
                  | space
                  | Word
@@ -53,11 +60,7 @@ def p_expresion_semicolon(p):
                  | Empty '''
     
     if len(p) > 2:
-        if (p[2] == ':='):
-            p[0] = Asignation("Asignacion: ", p[1], p[3])
-            print("asignacion nivel: "+p[1].type +","+str(p[3].type))
-
-        elif (p[2] == ';'):
+        if (p[2] == ';'):
             p[0] = Secuencia("Secuencia", p[1],p[3])
             print("semicolon nivel: "+p[1].type + ";" + p[3].type)
     else:
@@ -72,6 +75,14 @@ def p_expresion_two_point(p):
     
     p[0] = TwoPoints("TwoPoints: ", p[1], p[3])
     print("dos puntos nivel: "+p[1].type +","+ p[3].type) 
+
+# Produccion para detectar la expresion no terminal asig
+def p_expresion_asig(p):
+    '''asig : expresion TkAsig expresion
+            | expresion TkAsig number'''
+    
+    p[0] = Asignation("Asignacion: ", p[1], p[3])
+    print("asignacion nivel: "+p[1].type +","+str(p[3].type))
 
 # Produccion para detectar la expresion no terminal Comma
 def p_expresion_comma(p):
@@ -102,14 +113,14 @@ def p_expresion_space_empty(p):
 
 # Produccion para detectar el bucle for
 def p_for(p):
-    '''bucle : TkFor condicion TkArrow expresion TkRof'''
+    '''bucle : TkFor cota TkArrow expresion TkRof'''
     p[0] = Loop_For("For: ", p[2], p[4])
     print("For: "+p[2].type +","+ p[4].type) 
 
 # Produccion para detectar condicion In y To
-def p_expresion_condicion(p):
-    '''condicion : expresion TkIn expresion
-                 | expresion TkTo expresion'''
+def p_expresion_cota(p):
+    '''cota : expresion TkIn expresion
+            | expresion TkTo expresion'''
     
     if (p[2] == 'in'):
         p[0] = Loop_For("In ", p[1], p[3])
@@ -118,10 +129,71 @@ def p_expresion_condicion(p):
         p[0] = Loop_For("To ", p[1], p[3])
         print("To: "+p[1].type +","+ p[3].type)
 
+# Produccion para detectar los if
+def p_if(p):
+    '''condition : TkIf guard TkFi'''
+    p[0] = Condition_If("If: ", p[2])
+    print("If: "+p[2].type)
+
+def p_guard(p):
+    '''guard : guard TkGuard guard
+             | arrow'''
+    if (len(p) > 2):
+        p[0] = Guard("Guard", p[1], p[3])
+        print("Arrow: "+p[1].type +","+ p[3].type)
+    else:
+        p[0] = Guard("Guard", p[1])
+        print("Guard: "+p[1].type)
+
+# Produccion para detectar -->
+def p_arrow(p):
+    '''arrow : booleans TkArrow expresion'''
+
+    p[0] = Arrow("Then", p[1], p[3])
+    print("Arrow: "+p[1].type +","+ p[3].type)
+
+# Produccion para detectar condiciones bool
+def p_booleans(p):
+    '''booleans : expresion TkAnd expresion
+               | expresion TkOr expresion
+               | expresion TkLess expresion
+               | expresion TkLeq expresion
+               | expresion TkGeq expresion
+               | expresion TkGreater expresion
+               | expresion TkEqual expresion
+               | expresion TkNEqual expresion'''
+    
+    if (p[2] == '/\\'):
+        p[0] = Condition("And: ", p[1], p[3])
+        print("And: "+p[1].type +","+ p[3].type)
+    elif (p[2] == '\\/'):
+        p[0] = Condition("Or: ", p[1], p[3])
+        print("Or: "+p[1].type +","+ p[3].type)
+    elif (p[2] == '<'):
+        p[0] = Condition("Less: ", p[1], p[3])
+        print("Less: "+p[1].type +","+ p[3].type)
+    elif (p[2] == '<='):
+        p[0] = Condition("Leq: ", p[1], p[3])
+        print("Leq: "+p[1].type +","+ p[3].type)
+    elif (p[2] == '>='):
+        p[0] = Condition("Geq: ", p[1], p[3])
+        print("Geq: "+p[1].type +","+ p[3].type)
+    elif (p[2] == '>'):
+        p[0] = Condition("Greater: ", p[1], p[3])
+        print("Greater: "+p[1].type +","+ p[3].type)
+    elif (p[2] == '=='):
+        p[0] = Condition("Equal: ", p[1], p[3])
+        print("Equal: "+p[1].type +","+ p[3].type)
+    else:
+        p[0] = Condition("NEqual: ", p[1], p[3])
+        print("NEqual: "+p[1].type +","+ p[3].type)
+
 # Produccion para detectar las palabras reservadas
 def p_reserved(p):
     '''reserved : TkInt
                 | TkBool
+                | TkTrue
+                | TkFalse
                 | TkArray readArray'''
     
     if (len(p) > 2):
@@ -130,9 +202,26 @@ def p_reserved(p):
     elif (p[1] == 'int'):
         p[0] = Reserved("TkInt: ", value = p[1])
         print("Reservado TkInt: "+ p[1])
+    elif (p[1] == 'true'):
+        p[0] = Reserved("TkTrue: ", value = p[1])
+        print("Reservado true: "+ p[1])
+    elif (p[1] == 'false'):
+        p[0] = Reserved("TkFalse: ", value = p[1])
+        print("Reservado false: "+ p[1])
     else:
         p[0] = Reserved("TkBool: ", value = p[1])
         print("Reservado bool: "+ p[1])
+
+def p_bool(p):
+    '''bools : TkTrue
+             | TkFalse'''
+    
+    if (p[1] == 'true'):
+        p[0] = Reserved("TkTrue: ", value = p[1])
+        print("Reservado true: "+ p[1])
+    else:
+        p[0] = Reserved("TkFalse: ", value = p[1])
+        print("Reservado false: "+ p[1])
 
 # Produccion para leer un array
 def p_read_array(p):
@@ -162,6 +251,7 @@ def p_write_array(p):
 def p_expresion_print(p):
     '''print : TkPrint expresion'''
     p[0] = toAtom("Print", p[2])
+    print("Print: "+ p[2].type)
 
 # Produccion para detectar la expresion terminal de un numero
 def p_number(p):
@@ -262,13 +352,14 @@ class Secuencia:
         pila = deque()
         ret = "-"*level + self.type 
         print(ret)
-        #print("Este es el tipo del hijo:"+str(self.right.type))
+        #print("Este es el tipo del hijo:"+str(self.type))
         #print("Este es el tipo del hijo derecho :"+str(self.right.type))
         #print("Este es el tipo del hijo izquierdo :"+str(self.left.type))
         if (self.right.type == "Secuencia"):
             pila.append(self.left)
             #print("pila en el nivel "+ str(self.type)+": "+ str(pila))
             pila += self.right.print_AST_DQ(level+1)
+            #print(len(pila))
             #print("pila en el nivel "+ str(self.type)+": "+ str(pila))
             while(len(pila)>0):
                 level = len(pila)
@@ -278,6 +369,8 @@ class Secuencia:
                 else:
                     x.print_AST(level+1)
         else:
+            #print("Pase por aqui")
+            
             self.left.print_AST(level+1)
             self.right.print_AST(level+1)
 
@@ -463,6 +556,102 @@ class WriteArray:
         pila.append(")")
         return pila
     
+class Condition_If:
+
+
+    def __init__(self,type, children = None,level = 0 ):
+        self.type = type
+        self.level = level
+        self.children = children
+
+    def print_AST(self, level=0):
+        ret = "-"*level + self.type 
+        print(ret)
+        self.children.print_AST(level+1)
+
+class Guard:
+
+    def __init__(self, type, left=None, right=None):
+        self.type = type
+        self.left = left
+        self.right = right
+        #self.value = value
+
+    def print_AST(self, level=0):
+        pila = deque()
+        #print("Este es el tipo del hijo:"+str(self.right.type))
+        #print("Este es el tipo del hijo derecho :"+str(self.right.type))
+        #print("Este es el tipo del hijo izquierdo :"+str(self.left.type))
+        if (self.right != None and self.right.type == "Guard" ):
+            pila.append(self.left)
+            #print("pila en el nivel "+ str(self.type)+": "+ str(pila))
+            pila += self.right.print_AST_DQ(level+1)
+            #print("pila en el nivel "+ str(self.type)+": "+ str(pila))
+            #print(len(pila))
+            for x in pila:
+                if(x == None):
+                    continue
+                x.print_AST(level+1)
+        else:
+            if (self.left != None):
+                self.left.print_AST(level+1)
+            if (self.right != None):
+                self.right.print_AST(level+1)
+
+    def print_AST_DQ(self,level=0):
+        pila = deque()
+        ret = "-"*level + self.type 
+        print(ret)
+        #print("Este es el tipo del hijo derecho DQ:"+str(self.right.type))
+        #print("Este es el tipo del hijo izquierdo DQ:"+str(self.left.type))
+        if (self.left != None and self.left.type == "Guard"):
+            #print("pila en el nivel "+ str(self.type)+": "+ str(pila))
+            pila += self.left.print_AST_DQ(level+1)
+        else:
+            pila.append(self.left)
+
+        if (self.right != None and self.right.type == "Guard"):
+            #print("pila en el nivel "+ str(self.type)+": "+ str(pila))
+            pila += self.left.print_AST_DQ(level+1)
+        else:
+            pila.append(self.right)
+        #print("pila en el nivel DQ "+ str(self.type)+": "+ str(pila))
+        return pila
+
+class Arrow:
+
+    def __init__(self, type, left=None, right=None):
+        self.type = type
+        self.left = left
+        self.right = right
+
+    def print_AST(self, level=0):
+        #res = "Estoy en AST FOR, el hijo izquierdo es: "+ str(self.left)
+        #print(res) 
+        #res = "Estoy en AST FOR, el hijo derecho es: "+ str(self.right)
+        #print(res)
+        ret = "-"*level + self.type
+        print(ret)
+        self.left.print_AST(level+1)
+        self.right.print_AST(level+1)
+
+class Condition:
+
+    def __init__(self, type, left=None, right=None):
+        self.type = type
+        self.left = left
+        self.right = right
+
+    def print_AST(self, level=0):
+        #res = "Estoy en AST FOR, el hijo izquierdo es: "+ str(self.left)
+        #print(res) 
+        #res = "Estoy en AST FOR, el hijo derecho es: "+ str(self.right)
+        #print(res)
+        ret = "-"*level + self.type
+        print(ret)
+        self.left.print_AST(level+1)
+        self.right.print_AST(level+1)
+
 class Loop_For:
 
     def __init__(self, type, left=None, right=None):
